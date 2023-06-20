2326e866f8e1cb8c343d41742a01ec745dacd8cd
0benzo <dabenzo24@gmail.com>
2023-06-20 03:00:40 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,13 @@
-#include "main.h"
-
-/**
- * print_alphabet - print all alphabet in lowercase
+/*
+ * File: 1-alphabet.c
+ * Auth: Brennan D Baraban
  */
 
+#include "holberton.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ */
 void print_alphabet(void)
 {
 	char letter;
